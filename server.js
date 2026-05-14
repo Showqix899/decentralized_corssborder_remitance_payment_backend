defda05b684f31af6e9328dab23c6cdcf9b283d4
showqi <mshowqi12@gmail.com>
2026-05-15 00:52:52 +0600

    database and basic project setup done

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./src/config/db.js";
+
+
+dotenv.config();
+
+//mongod db connect 
+connectDB()
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("XRPL Remittance Backend Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
